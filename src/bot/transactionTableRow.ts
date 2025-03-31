125c220eda7c47171d6876030aad9dc4bd33c1dd
Sergey Bellov <bellov.pro@gmail.com>
2025-04-01 00:09:33 +0300

    My columns added

--- src/bot/transactionTableRow.ts
+++ src/bot/transactionTableRow.ts
@@ -2,23 +2,32 @@ import { format, parseISO } from "date-fns";
 import { systemName } from "../config.js";
 import type { TransactionRow } from "../types.js";
 import { normalizeCurrency } from "../utils/currency.js";
+import { v4 as uuidv4 } from 'uuid';
 
 const currentDate = format(Date.now(), "yyyy-MM-dd");
 const { TRANSACTION_HASH_TYPE } = process.env;
 
 export const TableHeaders = [
   "date",
+  "processedDate",
   "amount",
+  "originalCurrency",
+  "chargedCurrency",
   "description",
   "memo",
   "category",
   "account",
   "hash",
+  "type",
+  "installments num",
+  "installments total",
   "comment",
   "scraped at",
   "scraped by",
   "identifier",
-  "chargedCurrency",
+  "status",
+  "companyId",
+  "UUID"
 ] as const;
 
 export type TableRow = Omit<
@@ -28,7 +37,7 @@ export type TableRow = Omit<
   amount: number;
 };
 
-export function tableRow(tx: TransactionRow): TableRow {
+/* export function tableRow(tx: TransactionRow): TableRow {
   return {
     date: format(parseISO(tx.date), "dd/MM/yyyy", {}),
     amount: tx.chargedAmount,
@@ -45,5 +54,33 @@ export function tableRow(tx: TransactionRow): TableRow {
     chargedCurrency:
       normalizeCurrency(tx.chargedCurrency) ||
       normalizeCurrency(tx.originalCurrency),
+  };
+}
+ */
+export function tableRow(tx: TransactionRow): TableRow {
+  var _a, _b, _c, _d;
+  return {
+    date: format(parseISO(tx.date), "dd/MM/yyyy", {}),
+    processedDate: format(parseISO(tx.processedDate), "dd/MM/yyyy", {}),
+    amount: tx.chargedAmount,
+    originalCurrency: tx.originalCurrency,
+    chargedCurrency:
+      normalizeCurrency(tx.chargedCurrency) ||
+      normalizeCurrency(tx.originalCurrency),
+    description: tx.description,
+    memo: (_a = tx.memo) !== null && _a !== void 0 ? _a : "",
+    category: (_b = tx.category) !== null && _b !== void 0 ? _b : "",
+    account: String(tx.account),
+    hash: tx.hash,
+    type: tx.type,
+    'installments num': (_d = tx.installments) !== null && _d !== void 0 ? _d.number : 0,
+    'installments total': (_d = tx.installments) !== null && _d !== void 0 ? _d.total : 0,
+    comment: "",
+    "scraped at": format(parseISO(currentDate), "dd/MM/yyyy HH:mm:ss", {}),
+    "scraped by": "bellov",
+    identifier: (_c = tx.identifier) !== null && _c !== void 0 ? _c : "",
+    status: tx.status,
+    companyId: tx.companyId,
+    UUID: uuidv4(),
   };
 }
